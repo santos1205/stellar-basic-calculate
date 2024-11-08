0ac9590700c5b75900cfcc1d20cf99fad130c9ce
mario santos <t765668971@cassi.com.br>
2024-11-08 20:53:41 -0300

    implementando as estruturas das operações

--- calculadora_soroban/contracts/calculadora/src/lib.rs
+++ calculadora_soroban/contracts/calculadora/src/lib.rs
@@ -1,9 +1,34 @@
 #![no_std]
 //use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
-use soroban_sdk::{contract, contractimpl, Env};
+use soroban_sdk::{contract, contractimpl, Env, Vec};
+
+// Estruturas do Enum e da Operação
+#[derive(Clone)]
+pub enum TipoOperacao {
+    Sum,
+    Sub,
+    Multi,
+    Div
+}
+
+#[derive(Clone)]
+pub struct Operacao {
+    pub x: u32,
+    pub y: u32,
+    pub result: u32,
+    pub op: TipoOperacao,
+    pub id_op: u32
+}
+
+impl Operacao {
+    pub fn new(x: u32, y: u32, result: u32, op: TipoOperacao, id_op: u32) -> Self {
+        Operacao { x, y, result, op, id_op }
+    }
+}
 
 #[contract]
 pub struct CalculadoraContract;
+
 
 #[contractimpl]
 impl CalculadoraContract {
