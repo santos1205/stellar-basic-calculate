d36f04476972087245c4ce757f339e2a42659ac6
mario santos <t765668971@cassi.com.br>
2024-11-09 16:40:29 -0300

    adicionando as operações de log

--- calculadora_soroban/contracts/calculadora/src/lib.rs
+++ calculadora_soroban/contracts/calculadora/src/lib.rs
@@ -1,49 +1,108 @@
 #![no_std]
-//use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
-use soroban_sdk::{contract, contractimpl, Env, Vec};
+use soroban_sdk::{contract, contractimpl, contracttype, Env, Symbol, Vec};
 
 // Estruturas do Enum e da Operação
-#[derive(Clone)]
+#[contracttype]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum TipoOperacao {
     Sum,
     Sub,
     Multi,
-    Div
+    Div,
 }
 
-#[derive(Clone)]
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Operacao {
     pub x: u32,
     pub y: u32,
     pub result: u32,
     pub op: TipoOperacao,
-    pub id_op: u32
+    pub id_op: u32,
 }
 
 impl Operacao {
     pub fn new(x: u32, y: u32, result: u32, op: TipoOperacao, id_op: u32) -> Self {
-        Operacao { x, y, result, op, id_op }
+        Operacao {
+            x,
+            y,
+            result,
+            op,
+            id_op,
+        }
     }
 }
 
 #[contract]
 pub struct CalculadoraContract;
 
-
 #[contractimpl]
 impl CalculadoraContract {
+    // ### Funções dos registros das operações
+    // Função que incrementa o id da operação
+    fn get_next_id(env: &Env) -> u32 {
+        let current_id: u32 = env
+            .storage()
+            .instance()
+            .get(&Symbol::new(env, "operation_id"))
+            .unwrap_or(0); // Se não encontrar, começa com 0
+        let next_id = current_id + 1;
+        env.storage()
+            .instance()
+            .set(&Symbol::new(env, "operation_id"), &next_id);
+
+        next_id
+    }
+
+    // Função para adicionar uma operação ao histórico
+    fn add_operation(env: &Env, op: Operacao) {
+        let key = Symbol::new(env, "operacoes");
+
+        // Carrega o histórico de operações ou inicia um novo vetor se estiver vazio
+        let mut operacoes: Vec<Operacao> = env
+            .storage()
+            .instance()
+            .get::<Symbol, Vec<Operacao>>(&Symbol::new(env, "operacoes"))
+            .unwrap_or(Vec::new(env));
+        operacoes.push_back(op);
+
+        // Atualiza o histórico no armazenamento
+        env.storage().instance().set(&key, &operacoes);
+    }
+
+    // Função para retornar todas as operações
+    pub fn all_op(env: Env) -> Vec<Operacao> {        
+        env
+            .storage()
+            .instance()
+            .get(&Symbol::new(&env, "operacoes"))
+            .unwrap_or(Vec::new(&env))
+    }
+
+    // ### Funções das operações
     // Função de soma
-    pub fn sum(_env: Env, x: u32, y: u32) -> u32 {
-        x + y
-    }    
+    pub fn sum(env: &Env, x: u32, y: u32) -> u32 {
+        let result = x + y;
+        // Obtenha o próximo ID auto-incrementado
+        let id = get_next_id(env);
+        let operacao = Operacao::new(x, y, result, TipoOperacao::Sum, id);
+        Self::add_operation(&env, operacao);
+        result
+    }
 
     // Função de subtração
-    pub fn sub(_env: Env, x: u32, y: u32) -> u32 {
+    pub fn sub(env: &Env, x: u32, y: u32) -> u32 {
+        let result: u32;
         if x > y {
-            x - y
+            result = x - y;
         } else {
-            y - x
+            result = y - x;
         }
+        let id = get_next_id(env);
+        let operacao = Operacao::new(x, y, result, TipoOperacao::Sub, id);
+        Self::add_operation(env, operacao);
+
+        result
     }
 
     // Função de multiplicação
--- calculadora_soroban/contracts/calculadora/src/test.rs
+++ calculadora_soroban/contracts/calculadora/src/test.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{Env};
+use soroban_sdk::Env;
 
 #[test]
 fn test_sum() {
@@ -36,4 +36,28 @@ fn test_div_por_zero() {
     let env = Env::default();
     let result = CalculadoraContract::div(env, 8, 0);
     assert_eq!(result, u32::MAX);
+}
+
+#[test]
+fn test_operations_listing() {
+    let env = Env::default();
+    let calc = CalculadoraContract::new(&env);
+
+    // Realizar algumas operações
+    calc.sum(&env, 5, 3);
+    calc.sub(&env, 10, 4);
+    calc.mul(&env, 6, 7);
+    calc.div(&env, 15, 5);
+
+    // Testa listagem de todas as operações
+    let all_ops = calc.all_op(&env);
+    assert_eq!(all_ops.len(), 4); // Espera-se que haja 4 operações registradas
+
+    // Verificação específica de operações
+    assert_eq!(all_ops[0].x, 5);
+    assert_eq!(all_ops[0].y, 3);
+    assert_eq!(all_ops[0].result, 8); // 5 + 3
+    assert_eq!(all_ops[1].result, 6); // 10 - 4
+    assert_eq!(all_ops[2].result, 42); // 6 * 7
+    assert_eq!(all_ops[3].result, 3); // 15 / 5
 }
