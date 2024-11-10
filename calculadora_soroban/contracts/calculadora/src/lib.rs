4d1e64f369621f674d566177dcf6d900208d90ca
mario santos <t765668971@cassi.com.br>
2024-11-09 21:30:35 -0300

    retornando todas as operações no teste

--- calculadora_soroban/contracts/calculadora/src/lib.rs
+++ calculadora_soroban/contracts/calculadora/src/lib.rs
@@ -1,15 +1,90 @@
 #![no_std]
 //use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
-use soroban_sdk::{contract, contractimpl, Env};
+use soroban_sdk::{contract, contractimpl, contracttype, map, symbol_short, Env, Map, Symbol, Vec};
+
+const OPERACOES: Symbol = symbol_short!("OPERACOES");
+const TTL_THRESHOLD: u32 = 1000;
+const TTL_EXTEND_TO: u32 = 5000;
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum OP {
+    Sum,
+    Sub,
+    Mul,
+    Div,
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Operation {
+    pub id: u32,
+    pub x: u32,
+    pub y: u32,
+    pub result: u32,
+    pub tipo_op: OP,    
+}
 
 #[contract]
 pub struct CalculadoraContract;
 
 #[contractimpl]
 impl CalculadoraContract {
+    // ### FUNÇÕES DOS REGISTROS DAS OPERAÇÕES
+    fn add_operacao(env: Env, op: Operation) -> u32 {
+        let mut ops: Map<u32, Operation> = env.storage().instance().get(&OPERACOES).unwrap_or(map![&env,]);
+
+        let novo_id = ops.len() as u32 + 1;
+        let nova_op = Operation {
+            id: op.id,
+            x: op.x,
+            y: op.y,
+            tipo_op: op.tipo_op,
+            result: op.result
+        };
+
+        ops.set(op.id, nova_op);
+        env.storage().instance().set(&OPERACOES, &ops);
+        env.storage().instance().extend_ttl(TTL_THRESHOLD, TTL_EXTEND_TO);
+
+        novo_id    
+    }
+
+    pub fn get_all_operacoes(env: Env) -> Vec<Operation> {
+        env.storage()
+            .instance()
+            .extend_ttl(TTL_THRESHOLD, TTL_EXTEND_TO);
+
+        env.storage()
+            .instance()
+            .get::<Symbol, Map<u32, Operation>>(&OPERACOES)
+            .unwrap_or(map![&env,])
+            .values()
+    }
+
+    // ### FUNÇÕES DAS OPERAÇÕES BÁSICAS    
     // Função de soma
-    pub fn sum(_env: Env, x: u32, y: u32) -> u32 {
-        x + y
+    pub fn sum(env: Env, x: u32, y: u32) -> u32 {
+        let result: u32;
+        result = x + y;
+
+        let nova_op = Operation {
+            id: 0,
+            x,
+            y,
+            result,
+            tipo_op: OP::Sum
+        };
+        
+        //Self::add_operacao(_env, nova_op);
+        let mut ops: Map<u32, Operation> = env.storage().instance().get(&OPERACOES).unwrap_or(map![&env,]);
+        let novo_id = ops.len() as u32 + 1;        
+
+        ops.set(novo_id, nova_op);
+        env.storage().instance().set(&OPERACOES, &ops);
+        env.storage().instance().extend_ttl(TTL_THRESHOLD, TTL_EXTEND_TO);
+
+        result
     }    
 
     // Função de subtração
@@ -22,7 +97,7 @@ impl CalculadoraContract {
     }
 
     // Função de multiplicação
-    pub fn mult(_env: Env, x: u32, y: u32) -> u32 {
+    pub fn mul(_env: Env, x: u32, y: u32) -> u32 {
         x * y
     }
 
--- calculadora_soroban/contracts/calculadora/src/test.rs
+++ calculadora_soroban/contracts/calculadora/src/test.rs
@@ -1,39 +1,32 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{Env};
+use soroban_sdk::Env;
 
 #[test]
-fn test_sum() {
+fn add_operacao() {
     let env = Env::default();
-    let result = CalculadoraContract::sum(env, 2, 3);
-    assert_eq!(result, 5);
+    let contract_id = env.register_contract(None, CalculadoraContract);
+    let client = CalculadoraContractClient::new(&env, &contract_id);
+
+    let x: u32 = 10;
+    let y: u32 = 5;
+    let result = client.sum(&x, &y);
+    assert_eq!(result, 15);
 }
 
 #[test]
-fn test_sub() {
-    let env: Env = Env::default();
-    let result = CalculadoraContract::sub(env, 10, 4);
-    assert_eq!(result, 6);
-}
-
-#[test]
-fn test_mult() {
-    let env: Env = Env::default();
-    let result = CalculadoraContract::mult(env, 5, 5);
-    assert_eq!(result, 25);
-}
-
-#[test]
-fn test_div() {
+fn get_all_operations() {
     let env = Env::default();
-    let result = CalculadoraContract::div(env, 10, 2);
-    assert_eq!(result, 5);
-}
+    let contract_id = env.register_contract(None, CalculadoraContract);
+    let client = CalculadoraContractClient::new(&env, &contract_id);
 
-#[test]
-fn test_div_por_zero() {
-    let env = Env::default();
-    let result = CalculadoraContract::div(env, 8, 0);
-    assert_eq!(result, u32::MAX);
+    client.sum(&10, &20);
+    client.sum(&1, &2);
+    client.sum(&2, &3);
+
+    let ops: Vec<Operation> = client.get_all_operacoes();
+    assert_eq!(ops.len(), 3);
+
+    // MY-TODO: VAMOS PEGAR ESSA LISTA E CONFERIR TODAS AS D+ OPERAÇÕES.
 }
